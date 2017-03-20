983d11b1422bca942d6d40806e9e63f423c5022b
fishsimon <1047607438@qq.com>
2017-03-20 20:08:11 +0800

    javascript

---
+++ views/Javascript/01/practice.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/3/20 0020.
+ */
+var time=100;
+if(time<10)
+{
+    console.log("Good day");
+}
+else if(time>=10&&time<20){
+    console.log("Good evening");
+}
+else{
+    console.log("ARE YOU DIE?");
+}
+
+var num=1;
+if(num===1){
+    console.log("num is equal to 1")
+}
+
+var month=3;
+if(month=3){
+    console.log("March");
+}
